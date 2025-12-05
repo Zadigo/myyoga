a2aa4a7f523f52cd185bf69c300d4c29e8debe7a
Zadigo <pendenquejohn@gmail.com>
2025-12-05 15:17:58 +0100

    Refactor tests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -8,14 +8,16 @@ export default defineConfig({
         test: {
           name: 'unit',
           include: ['test/{e2e,unit}/*.{test,spec}.ts'],
-          environment: 'node'
+          environment: 'node',
+          testTimeout: 10000
         }
       },
       await defineVitestProject({
         test: {
           name: 'nuxt',
           include: ['test/nuxt/*.{test,spec}.ts'],
-          environment: 'nuxt'
+          environment: 'nuxt',
+          testTimeout: 20000
         }
       })
     ]
